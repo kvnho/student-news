ebaab9aa0ec3d5e90a516e9cff6635785b4ae4fa
Kevin <kevinho479@gmail.com>
2020-04-17 19:20:02 -0400

    added firebase, refactored headers

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyC97PQaG2sbUq47Z0-wzlNx59Saruk_P8Q',
+	authDomain: 'student-news-43aa5.firebaseapp.com',
+	databaseURL: 'https://student-news-43aa5.firebaseio.com',
+	projectId: 'student-news-43aa5',
+	storageBucket: 'student-news-43aa5.appspot.com',
+	messagingSenderId: '1046155170763',
+	appId: '1:1046155170763:web:3319c5fe2f1d87b019dc4e',
+};
+
+export default firebaseConfig;
